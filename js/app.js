58abd9b869d95365f76e6d2e2ec180c6d576f57f
CyrilDenoyelle <Cyril-denoyelle@hotmail.fr>
2017-02-12 00:51:10 +0100

    ajout de .on submit et vidage de l'input quand on envoi

--- js/app.js
+++ js/app.js
@@ -1,53 +1,40 @@
 $(document).ready(function() {
 
+	// $('#namelistform').on
 
-    $("#add").click(function() {
-        var add = $("#todox").val();
-        $("#todolist").append("<li class='nonvalide'>" + add + "</li>")
-        $('#todolist').append($("<button class='validage nonvalide'>x</button>").click(function() {
-            var ca = $(this).prev();
-            ca.toggleClass("valide");
-            $(this).toggleClass("valide");
-            ca.toggleClass("nonvalide");
-            $(this).toggleClass("nonvalide");
-        }));
+	$("#todolist").on('submit', function(e) {
+		e.preventDefault();
+		var add = $("#todoinp").val();
+		$("#todoinp").val('');
+		$("#todolistcont").append("<li class='nonvalide'>" + add + "</li>");
+		$('#todolistcont').append($("<button class='validage nonvalide'>x</button>").click(function() {
+			var prev = $(this).prev();
+			prev.toggleClass("valide");
+			$(this).toggleClass("valide");
+			prev.toggleClass("nonvalide");
+			$(this).toggleClass("nonvalide");
+		}));
+	});
 
-    });
+	$("#todo").click(function() {
+		var nonvalide = $(".nonvalide");
+		var valide = $(".valide");
+		valide.addClass("cach");
+		nonvalide.removeClass("cach");
+	});
 
-    $(".validage").click(function() {
-        var ca = $(this).prev();
-        ca.toggleClass("valide");
-        $(this).toggleClass("valide");
-        ca.toggleClass("nonvalide");
-        $(this).toggleClass("nonvalide");
-    });
+	$("#done").click(function() {
+		var valide = $(".valide");
+		var nonvalide = $(".nonvalide");
+		nonvalide.addClass("cach");
+		valide.removeClass("cach");
+	});
 
-
-
-    $("#todo").click(function() {
-        var nonvalide = $(".nonvalide");
-        var valide = $(".valide");
-        valide.addClass("cach");
-        nonvalide.removeClass("cach");
-
-    })
-    $("#done").click(function() {
-        var valide = $(".valide");
-        var nonvalide = $(".nonvalide");
-        nonvalide.addClass("cach");
-        valide.removeClass("cach");
-
-    })
-    $("#all").click(function() {
-        var valide = $(".valide");
-        var nonvalide = $(".nonvalide");
-        nonvalide.removeClass("cach");
-        valide.removeClass("cach");
-
-
-
-
-    })
-
+	$("#all").click(function() {
+		var valide = $(".valide");
+		var nonvalide = $(".nonvalide");
+		nonvalide.removeClass("cach");
+		valide.removeClass("cach");
+	})
 
 });
