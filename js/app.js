31d30656a508f822aa760c768c51e392eb348c36
CyrilDenoyelle <Cyril-denoyelle@hotmail.fr>
2017-02-12 17:55:51 +0100

    pofinage bouton affichage

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
 		e.preventDefault();
 		var add = $('#listnameinp').val();
 		$('#listnameinp').val('');
-		$('#total').append($('<div class="container list"><button class="suppr">X</button><span class="titres">'+ add +'</span><form class="todolist"><input class="todoinp" placeholder="Entrez une tâche à effectuer" /><input type="submit" class="Ajoutage add" value="Ajouter" /></form><ul class="todolistcont"></ul><button type="button" class="btn btn-primary todo">tâches à faire</button><button type="button" class="btn btn-primary done">tâches faite</button><button type="button" class="btn btn-primary all">Toutes les tâches</button></div>'));
+		$('#total').append($('<div class="list"><button class="suppr">X</button><span class="titres">'+ add +'</span><form class="todolist"><input class="todoinp" placeholder="Entrez une tâche à effectuer" /><input type="submit" class="Ajoutage add" value="Ajouter" /></form><ul class="todolistcont"></ul><button type="button" class="btn btn-primary todo">tâches à faire</button><button type="button" class="btn btn-primary done">tâches faite</button><button type="button" class="btn btn-primary all">Toutes les tâches</button></div>'));
 
 		$(".todolist").each(function(){
 			if(!$(this).attr('ok')){
